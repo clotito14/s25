1112b5b0976c972d452ce985faad91b7aa0faf2f
Chase Lotito <clotito@generalkinematics.com>
2025-02-17 20:36:18 -0600

    ECE478 Labs etc...

--- COMMUNICATIONS/LABS/2/lab2.m
+++ COMMUNICATIONS/LABS/2/lab2.m
@@ -5,6 +5,7 @@ Fs = 200;               % SAMPLE FREQ.
 t = -2:1/Fs:2;          % LINEAR SPACE FOR TIME
 T0 = 1;                 % WIDTH OF TRI PULSE
 x = tripuls(t, T0);     % GENERATE TRI PULSE
+xprime = tripuls(t/2, T0);
 
 % PLOT THE TRIANGULAR PULSE
 figure(1);
@@ -23,5 +24,53 @@ f = (0:nfft/2-1)*Fs/nfft; % FREQ. VECTOR
 figure(2);
 plot(f, mx);
 title('Amplitude Spectrum of \Delta(t)');
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+
+% MODULATE VIA FREQ TRANSLATION
+fc = 50;
+y = x .* cos(2 * pi * fc * t);
+yprime = xprime .* cos(2 * pi * fc * t);
+
+% PLOT MODULATED TIME DOMAIN SIGNAL
+figure(3);
+plot(t,y);
+title('\Delta(t)cos(2\pif_ct)');
+xlabel('Time (sec)');
+ylabel('Amplitude');
+
+nfft = 1024;            % FFT LENGTH
+Y = fft(y, nfft);       % PERFORM FFT on x(t)
+Y = Y(1:nfft/2);        % TAKE POSITIVE SPECTRUM`
+my = abs(Y/nfft);       % AMPLITUDE SPECTRUM
+f = (0:nfft/2-1)*Fs/nfft; % FREQ. VECTOR
+
+% PLOT MODULATED AMPLITUDE SPECTRUM
+figure(4);
+plot(f, my);
+title('Amplitude Spectrum of \Delta(t)cos(2\pif_ct)');
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+
+
+% FOR THE /\(T/2) SIGNAL 
+
+% PLOT MODULATED TIME DOMAIN SIGNAL
+figure(5);
+plot(t,yprime);
+title('\Delta(t/2)cos(2\pif_ct)');
+xlabel('Time (sec)');
+ylabel('Amplitude');
+
+nfft = 1024;            % FFT LENGTH
+Yprime = fft(yprime, nfft);       % PERFORM FFT on x(t)
+Yprime = Yprime(1:nfft/2);        % TAKE POSITIVE SPECTRUM`
+myprime = abs(Yprime/nfft);       % AMPLITUDE SPECTRUM
+f = (0:nfft/2-1)*Fs/nfft; % FREQ. VECTOR
+
+% PLOT MODULATED AMPLITUDE SPECTRUM
+figure(6);
+plot(f, myprime);
+title('Amplitude Spectrum of \Delta(t/2)cos(2\pif_ct)');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
---
+++ COMMUNICATIONS/LABS/3/exercise1.m
@@ -0,0 +1,48 @@
+% Defining AM modulation Index
+mu = input('Enter the value of modulation index (mu) = ');
+
+% Defining frequency of message signal
+fm = input('Enter frequency of message signal (fm) (e.g. fm = 50) = ');
+
+% Defining frequency of carrier signal
+fc = input('Enter carrier frequency (fc) (fc>>fm) (e.g. fc = fm*10) = ');
+
+N = 1024;    % N point FFT N>fc to avoid freq domain aliasing
+fs = 4096;    % sample frequency
+t = (0:N-1)/fs;
+
+% Generating modulating signal (message signal)
+Am = 5;    % Amplitude of message signal
+mt = Am * cos(2 * pi * fm * t);    % generating the message signal
+figure(1)
+subplot(4,1,1);
+plot(t, mt), grid on;    % A plot for the message signal
+title('Message Signal');
+xlabel('time');
+ylabel('Amplitude');
+
+% Generating carrier signal
+Ac = 10;    % Amplitude of carrier signal
+ct = Ac * cos(2 * pi * fc * t);    % generation of carrier signal
+subplot(4,1,2);
+plot(t, ct), grid on;    % A plot for the carrier signal
+title('Carrier Signal');
+xlabel('time');
+ylabel('Amplitude');
+
+% Generating AM Modulated signal
+st = Ac * (1 + mu * cos(2 * pi * fm * t)) .* cos(2 * pi * fc * t);    % AM wave
+subplot(4,1,3);
+plot(t, st);    % a plot for the AM signal
+title('Amplitude Modulated signal');
+xlabel('time');
+ylabel('Amplitude');
+
+% Generating spectrum of AM wave
+Sf = 2 / N * abs(fft(st, N));
+f = fs * (0 : N/2) / N;    % fft is symmetric, only the positive half is sufficient
+subplot(4,1,4);
+plot(f(1:256), Sf(1:256));    % a plot for the AM signal
+title('Spectrum of AM signal');
+xlabel('Frequency');
+ylabel('Amplitude Spectrum');
